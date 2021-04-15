7e05aebfd58e1191956a5330643e8561f999cb97
Rahix <rahix@rahix.de>
2021-04-15 21:58:42 +0200

    hardware-tests: Add shiftreg test

---
+++ hardware-tests/src/bin/shiftreg-demo.rs
@@ -0,0 +1,69 @@
+#![no_std]
+#![no_main]
+
+use panic_rtt_target as _;
+use rtt_target::rprintln;
+
+use micromath::F32Ext as _;
+use stm32f3xx_hal::{self as hal, pac, prelude::*};
+
+#[cortex_m_rt::entry]
+fn main() -> ! {
+    rtt_target::rtt_init_print!();
+
+    let dp = pac::Peripherals::take().unwrap();
+    let cp = cortex_m::Peripherals::take().unwrap();
+
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+
+    let clocks = rcc
+        .cfgr
+        .use_hse(8u32.mhz())
+        .sysclk(48u32.mhz())
+        .pclk1(24u32.mhz())
+        .freeze(&mut flash.acr);
+
+    let mut delay = stm32f3xx_hal::delay::Delay::new(cp.SYST, clocks);
+
+    let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
+
+    let mut data = gpioa
+        .pa2
+        .into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
+    let mut dclk = gpioa
+        .pa6
+        .into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
+    let mut sclk = gpioa
+        .pa4
+        .into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
+
+    let mut frame = 0u64;
+    loop {
+        let value = (((frame as f32 / 6.0).sin() * 0.5 + 0.5) * 21.0) as u32;
+
+        rprintln!("{:6} - {:2}", frame, value);
+
+        for i in 0..20 {
+            if i < value {
+                data.set_high().unwrap();
+            } else {
+                data.set_low().unwrap();
+            }
+
+            dclk.set_high().unwrap();
+            dclk.set_low().unwrap();
+        }
+
+        sclk.set_high().unwrap();
+        sclk.set_low().unwrap();
+
+        delay.delay_ms(10u16);
+        frame += 1;
+    }
+}
+
+#[cortex_m_rt::exception]
+fn HardFault(ef: &cortex_m_rt::ExceptionFrame) -> ! {
+    panic!("Hard Fault: {:#?}", ef);
+}
